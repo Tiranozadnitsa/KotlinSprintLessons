59fb9c6a11f07bca48b80b97ac41be7512ae9438
Rey <tiranozadnitsa@gmail.com>
2025-04-03 17:43:46 +0300

    KS-1-3 corrected, for review

--- src/main/kotlin/lesson_1/lesson1_3.kt
+++ src/main/kotlin/lesson_1/lesson1_3.kt
@@ -2,18 +2,17 @@ package org.example.lesson_1
 
 fun main(){
     val year: Int = 1961
-    var hour: Int = 9 //По Мск
-    var minute: Int = 7
+    var hour: String = "09"
+    var minute: String = "07"
     println("$year")
     println("$hour")
     println("$minute")
-    hour = 10
-    minute = 55
+    hour = "10"
+    minute = "55"
 
-    //вывод как удобнее
     println("$hour:$minute")
 
-    //вывод по подсказке
+
     print("$hour")
     print(":")
     print("$minute")
