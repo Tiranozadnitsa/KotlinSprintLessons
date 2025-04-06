77f53aa2989926e32ff668913608bf5c65e10ba1
Rey <tiranozadnitsa@gmail.com>
2025-04-06 11:36:10 +0300

    KS-2-3 corrected, for review

--- src/main/kotlin/lesson_2/lesson_3.kt
+++ src/main/kotlin/lesson_2/lesson_3.kt
@@ -4,8 +4,9 @@ fun main() {
     var hoursOfDeparture: Int = 9
     var minutesOfDeparture: Int = 39
     var travelTime: Int = 457
+    val minutesInHour: Byte = 60
 
-    var hoursOfArrival: Int = (hoursOfDeparture * 60 + travelTime) / 60
+    var hoursOfArrival: Int = (hoursOfDeparture * minutesInHour + travelTime) / 60
     var minutesOfArrival: Int = (minutesOfDeparture + travelTime) % 60
 
     println("Время прибытия: $hoursOfArrival:$minutesOfArrival")
